d002eb0408118620e131414bd663fb5025c1c8b0
Pandam <victorvgv96@hotmail.es>
2023-03-04 14:49:20 +0100

    connected to mongodb with mongoose

--- connection.js
+++ connection.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 //Using process env we can use global parameters for the application
 //It's important to add a gitignore for the env file
 //We need to install dotenv dependency
-const dbURI = "mongodb+srv://user-notes:user123@cluster0.avehvpt.mongodb.net/notesdb?retryWrites=true&w=majority";
+const dbURI =
+  process.env.MONGO_DB_URI ||
+  "mongodb+srv://user-notes:user123@cluster0.avehvpt.mongodb.net/notesdb?retryWrites=true&w=majority";
 const db = mongoose.connection;
 
 mongoose.connect(dbURI).catch((err) => console.log(err));
 
+console.log(dbURI)
 //Event triggered on database connected and on error
 db.on("open", () => console.log("dataBase opened"));
 db.on("error", (err) => console.log(err));
